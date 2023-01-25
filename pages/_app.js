40fb3f2db2f4b78a688dae89a509ed4a85f360d1
ererobe95 <danielererobe@gmail.com>
2023-01-25 21:46:02 +0100

    community feed from stack overflow api

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
+import Header from '../components/Header/Header';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle` 
+  body { 
+    margin: 0; 
+    padding: 0; 
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", 
+"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", 
+sans-serif; 
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale; 
+  } 
+`;
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
